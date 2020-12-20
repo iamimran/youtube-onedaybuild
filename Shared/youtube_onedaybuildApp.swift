f9f62e3a2803a77029ee8ab60b47f2012ea3d8c2
Imran Ahmed <iam.imran@yahoo.com>
2020-12-20 22:27:40 +0500

    Initial Commit

---
+++ Shared/youtube_onedaybuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_onedaybuildApp.swift
+//  Shared
+//
+//  Created by Imran Ahmed on 20/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct youtube_onedaybuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
